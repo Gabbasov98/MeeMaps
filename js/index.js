81b1118141e78466c8e74c57358b485c27ee8a85
Marsel Gabbasov <marsel13_98@mail.ru>
2021-10-07 16:15:20 +0300

    edit

--- js/index.js
+++ js/index.js
@@ -8,4 +8,41 @@ $(document).ready(function() {
         console.log(path)
     })
 
+    $(".easy__tab").click(function() {
+        let path = $(this).attr("data-tab-path");
+        $(".easy__tab").removeClass("easy__tab--active");
+        $(this).addClass("easy__tab--active");
+        $(".easy__content").removeClass("easy__content--active");
+        $(`.easy__content[data-content-path="${path}"]`).addClass("easy__content--active");
+        $(".easy__pagination-item").removeClass("easy__pagination-item--active");
+        $(`.easy__pagination-item[data-pagination-path="${path}"]`).addClass("easy__pagination-item--active");
+        console.log(path)
+    })
+
+    setInterval(function() {
+        changeEasySlide()
+    }, 3000);
+
+    let path = parseInt($(".easy__tab--active").attr("data-tab-path"));
+
+    function changeEasySlide() {
+        if ((path >= 1) && (path < 3)) {
+            path = path + 1
+            $(".easy__tab").removeClass("easy__tab--active");
+            $(`.easy__tab[data-tab-path="${path}"]`).addClass("easy__tab--active");
+            $(".easy__content").removeClass("easy__content--active");
+            $(`.easy__content[data-content-path="${path}"]`).addClass("easy__content--active");
+            $(".easy__pagination-item").removeClass("easy__pagination-item--active");
+            $(`.easy__pagination-item[data-pagination-path="${path}"]`).addClass("easy__pagination-item--active");
+        } else if (path === 3) {
+            path = 1
+            $(".easy__tab").removeClass("easy__tab--active");
+            $(`.easy__tab[data-tab-path="${path}"]`).addClass("easy__tab--active");
+            $(".easy__content").removeClass("easy__content--active");
+            $(`.easy__content[data-content-path="${path}"]`).addClass("easy__content--active");
+            $(".easy__pagination-item").removeClass("easy__pagination-item--active");
+            $(`.easy__pagination-item[data-pagination-path="${path}"]`).addClass("easy__pagination-item--active");
+        }
+
+    }
 })
